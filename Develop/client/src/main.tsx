5fe2767a457dbe64846b2865e7b8e708c9c6500d
cjsquared-dev <camjohnson0618@gmail.com>
2024-11-21 18:17:08 -0700

    setup test login components

---
+++ Develop/client/src/main.tsx
@@ -0,0 +1,28 @@
+import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom' ;
+import App from './App.tsx';
+import Login from './pages/Login.tsx';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                index: true,
+                element: <HomePage />
+            },
+            {
+                path: '/login',
+                element: <Login />
+            }
+        
+]
+}
+])
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+    ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+}
